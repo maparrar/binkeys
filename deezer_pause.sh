80177c3e585d99d746dec14baa0057f92428d4e0
Alejandro Parra <maparrar@gmail.com>
2018-01-25 11:22:36 -0500

    Add spotify controls

--- deezer_pause.sh
+++ deezer_pause.sh
@@ -1,1 +1,2 @@
 xdotool search --name Deezer windowactivate key shift+ctrl+alt+space
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
