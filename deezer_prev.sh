6acf872cd25526eb1bbf68d39ceebe167a29e9ed
Alejandro Parra <maparrar@gmail.com>
2018-01-25 11:22:55 -0500

    Add spotify controls

--- deezer_prev.sh
+++ deezer_prev.sh
@@ -1,1 +1,2 @@
 xdotool search --name Deezer windowactivate key shift+ctrl+alt+Left
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
